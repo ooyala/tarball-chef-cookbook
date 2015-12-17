b4acc3f12811868608cfd737957c09a80ebaedf1
bbbco <bdgoad@gmail.com>
2015-12-17 12:38:25 -0500

    Refactor provider to make more DRY and variables more descriptive

--- providers/x.rb
+++ providers/x.rb
@@ -28,27 +28,36 @@ rescue StandardError => e
   raise e
 end
 
-def t_stream(f)
-  tgz = Zlib::GzipReader.new(f)
+def t_stream(tarball)
+  tarball_gz = Zlib::GzipReader.new(tarball)
 rescue Zlib::GzipFile::Error
   # Not gzipped
-  f.rewind
-  f
+  tarball.rewind
+  tarball
 else
-  tgz
+  tarball_gz
 end
 
-def mkdestdir(tarball)
-  directory tarball.destination do
+def mkdir(destination, owner = nil, group = nil, mode = nil)
+  return if destination.nil?
+  directory destination do
     action :create
-    owner tarball.owner
-    group tarball.group
-    # We use octal here for UNIX file mode readability, but we could just
-    # as easily have used decimal 511 and gotten the correct behavior
-    mode 0777 & ~tarball.umask.to_i
+    owner owner unless owner.nil?
+    group group unless group.nil?
+    mode mode unless mode.nil?
     recursive true
-    tarball.updated_by_last_action(true)
   end
+end
+
+def mkdestdir(tarball_resource)
+  dest = tarball_resource.destination
+  owner = tarball_resource.owner
+  group = tarball_resource.group
+  # We use octal here for UNIX file mode readability, but we could just
+  # as easily have used decimal 511 and gotten the correct behavior
+  mode = 0777 & ~tarball_resource.umask.to_i
+  mkdir(dest, owner, group, mode)
+  tarball_resource.updated_by_last_action(true)
 end
 
 # Placeholder method in case someone actually needs PAX support
@@ -56,125 +65,127 @@ def pax_handler(pax)
   Chef::Log.debug("PAX: #{pax}") if pax
 end
 
-def t_mkdir(tarball, entry, pax, name = nil)
+def t_mkdir(tarball_resource, entry, pax, name = nil)
   pax_handler(pax)
   if name.nil?
-    dir = ::File.join(tarball.destination, entry.full_name).gsub(%r{/$}, '')
+    dir = ::File.join(tarball_resource.destination, entry.full_name)
+    dir = dir.gsub(%r{/$}, '')
   else
     dir = name
   end
-  directory dir do
-    action :create
-    owner tarball.owner || entry.header.uid
-    group tarball.group || entry.header.gid
-    mode { (fix_mode(entry.header.mode) | 0111) & ~tarball.umask.to_i }
-    recursive true
-  end
+  return if ::File.directory?(dir)
+  owner = tarball_resource.owner || entry.header.uid
+  group = tarball_resource.group || entry.header.gid
+  mode = lambda do
+    (fix_mode(entry.header.mode) | 0111) & ~tarball_resource.umask.to_i
+  end.call
+
+  mkdir(dir, owner, group, mode)
 end
 
-def get_target(tarball, entry, type)
+def get_target(tarball_resource, entry, type)
   if type == :symbolic
     entry.header.linkname
   else
-    ::File.join(tarball.destination, entry.header.linkname)
+    ::File.join(tarball_resource.destination, entry.header.linkname)
   end
 end
 
-def t_link(tarball, entry, type, pax, longname)
+def t_link(tarball_resource, entry, type, pax, longname)
   pax_handler(pax)
-  dir = tarball.destination
-  t_mkdir(tarball, entry, pax, dir) unless ::File.exist?(dir)
-  target = get_target(tarball, entry, type)
+  dir = tarball_resource.destination
+  t_mkdir(tarball_resource, entry, pax, dir)
+  target = get_target(tarball_resource, entry, type)
 
   if type == :hard &&
-     !(::File.exist?(target) || tarball.created_files.include?(target))
+     !(::File.exist?(target) || tarball_resource.created_files.include?(target))
     Chef::Log.debug "Skipping #{entry.full_name}: #{target} not found"
     return
   end
 
   src = ::File.join(dir, longname || entry.full_name)
-  t_mkdir(tarball, entry, pax, ::File.dirname(src)) \
-      unless ::File.directory?(::File.dirname(src))
+  t_mkdir(tarball_resource, entry, pax, ::File.dirname(src))
   link src do
     to target
-    owner tarball.owner || entry.header.uid
+    owner tarball_resource.owner || entry.header.uid
     link_type type
     action :create
   end
 end
 
-def t_file(tarball, entry, pax, longname)
+def t_file(tarball_resource, entry, pax, longname)
   pax_handler(pax)
   file_name = longname || entry.full_name
   Chef::Log.info "Creating file #{file_name}"
-  fqpn = ::File.join(tarball.destination, file_name)
-  t_mkdir(tarball, entry, pax, ::File.dirname(fqpn)) \
-      unless ::File.directory?(::File.dirname(fqpn))
+  fqpn = ::File.join(tarball_resource.destination, file_name)
+  t_mkdir(tarball_resource, entry, pax, ::File.dirname(fqpn))
   file fqpn do
     action :create
-    owner tarball.owner || entry.header.uid
-    group tarball.group || entry.header.gid
-    mode fix_mode(entry.header.mode) & ~tarball.umask.to_i
+    owner tarball_resource.owner || entry.header.uid
+    group tarball_resource.group || entry.header.gid
+    mode fix_mode(entry.header.mode) & ~tarball_resource.umask.to_i
     sensitive true
     content entry.read
   end
-  tarball.created_files << fqpn
+  tarball_resource.created_files << fqpn
 end
 
-def on_list?(name, tarball)
-  Array(tarball.extract_list).each do |r|
-    return true if ::File.fnmatch?(r, name)
+def on_list?(filename, tarball_resource)
+  Array(tarball_resource.extract_list).each do |r|
+    return true if ::File.fnmatch?(r, filename)
   end
   false
 end
 
-def wanted?(name, tarball, type)
-  if ::File.exist?(::File.join(tarball.destination, name)) &&
-     tarball.overwrite == false
+def wanted?(filename, tarball_resource, type)
+  if ::File.exist?(::File.join(tarball_resource.destination, filename)) &&
+     tarball_resource.overwrite == false
     false
   elsif %w(2 5 L).include?(type)
     true
-  elsif tarball.extract_list
-    on_list?(name, tarball)
+  elsif tarball_resource.extract_list
+    on_list?(filename, tarball_resource)
   else
     true
   end
 end
 
-def t_extraction(tar, tarball)
+def t_extraction(tarball, tarball_resource)
   # pax and longname track extended types that span more than one tar entry
   pax = nil
   longname = nil
-  Gem::Package::TarReader.new(tar).each do |ent|
-    next unless wanted?(ent.full_name, tarball, ent.header.typeflag)
-    Chef::Log.info "Next tar entry: #{ent.full_name}"
-    case ent.header.typeflag
+  Gem::Package::TarReader.new(tarball).each do |entry|
+    unless wanted?(entry.full_name, tarball_resource, entry.header.typeflag)
+      next
+    end
+    Chef::Log.info "Next tar entry: #{entry.full_name}"
+    case entry.header.typeflag
     when '1'
-      t_link(tarball, ent, :hard, pax, longname)
+      t_link(tarball_resource, entry, :hard, pax, longname)
       pax = nil
       longname = nil
     when '2'
-      t_link(tarball, ent, :symbolic, pax, longname)
+      t_link(tarball_resource, entry, :symbolic, pax, longname)
       pax = nil
       longname = nil
     when '5'
-      t_mkdir(tarball, ent, pax)
+      t_mkdir(tarball_resource, entry, pax)
       pax = nil
       longname = nil
     when '3', '4', '6', '7'
-      Chef::Log.debug "Can't handle type for #{ent.full_name}: skipping"
+      Chef::Log.debug "Can't handle type for #{entry.full_name}: skipping"
       pax = nil
       longname = nil
     when 'x', 'g'
       Chef::Log.debug 'PaxHeader'
-      pax = ent
+      pax = entry
       longname = nil
     when 'L', 'K'
-      longname = ent.read.strip
+      longname = entry.read.strip
       Chef::Log.debug "Using LONG(NAME|LINK) #{longname}"
       pax = nil
     else
-      t_file(tarball, ent, pax, longname)
+      t_file(tarball_resource, entry, pax, longname)
       pax = nil
       longname = nil
     end
@@ -189,11 +200,11 @@ end
 provides :tarball if self.respond_to?('provides')
 
 action :extract do
-  tarball = new_resource
-  Chef::Log.info "TARFILE: #{tarball.source || tarball.name}"
-  tar = t_open(tarball.source || tarball.name)
-  tar = t_stream(tar)
-  mkdestdir(tarball)
-  t_extraction(tar, tarball)
+  tarball_resource = new_resource
+  Chef::Log.info "TARFILE: #{tarball_resource.source || tarball_resource.name}"
+  tarball = t_open(tarball_resource.source || tarball_resource.name)
+  tarball = t_stream(tarball)
+  mkdestdir(tarball_resource)
+  t_extraction(tarball, tarball_resource)
   new_resource.updated_by_last_action(true)
 end
